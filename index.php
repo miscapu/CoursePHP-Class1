a1312a26045a0533f499d75928915a62ecc4b675
miscapu <mignan2010@outlook.com>
2019-07-09 22:38:53 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First App</title>
+</head>
+
+<body>
+    <?php
+        echo "Hello, I am script PHP";
+    ?>
+</body>
+
+</html>
